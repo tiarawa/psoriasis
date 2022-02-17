faf764bf5b50c20998aedd462531a4d06527a558
tiaraxie <xtz19971009@outlook.com>
2022-02-17 16:24:19 +0800

    Update index.js

--- miniprogram/pages/detail/index.js
+++ miniprogram/pages/detail/index.js
@@ -12,36 +12,16 @@ Page({
     slider2: ['0 皮损与正常皮肤平齐', '1 皮损轻微高出于正常皮肤表面','2 中等度隆起 斑块的边缘为圆或斜坡型', '3 皮损肥厚 隆起明显','4 皮损高度增厚 隆起极为明显'],
     slider3: ['0 表面无可见鳞屑', '1 部分皮损表面上覆有鳞屑 以细微的鳞屑为主','2 大多数皮损表面完全或不完全覆有鳞屑 鳞屑呈片状', '3 几乎全部皮损表面覆有鳞屑 鳞屑较厚成层','4 全部皮损表面均覆有鳞屑 鳞屑很厚成层深'],
     showView:false,
-    headF:'',
-    headE:'',
-    headI:'',
-    headD:'',
+    head:'',
+    arm:'',
+    body:'',
+    leg:'',
     headPASI:'0.1',
     armPASI:'0.2',
     bodyPASI:'0.3',
     legPASI:'0.4',
-    totalPASI:''
   },
 
-  add:function(){
-    console.log("call add...")
-    wx.cloud.callFunction({
-      name:"add",
-      data:{
-        a: this.data.headE,
-        b: this.data.headI,
-        c: this.data.headD,
-      }
-    }).then( res => {
-      console.log(res.calResult)
-      this.setData({
-        calResult: res.result.calResult
-      })
-    })
-  },
-  mul:function(){
-
-  },
   onLoad(options) {
     // 保存上一页传来的 _id 字段，用于后续查询待办记录
     if (options.id !== undefined) {
@@ -62,11 +42,11 @@ Page({
       headE: e.detail.value
       })
     },
-  // 根据 _id 值查询并显示待办数据
+  // 根据 _id 值查询并显示数据
   async onShow() {
     if (this.data._id.length > 0) {
       const db = await getApp().database()
-      // 根据 _id 值查询数据库中对应的待办事项
+      // 根据 _id 值查询数据库中对应的事项
       db.collection(getApp().globalData.collection).where({
         _id: this.data._id
       }).get().then(res => {
@@ -76,7 +56,11 @@ Page({
         } = res
         // 将数据保存到本地、更新显示
         this.setData({
-          todo,
+          todo, 
+          head: this.data.headPASI * (todo.sli1 + todo.sli2 + todo.sli3) * todo.freq0,
+          arm: this.data.armPASI * (todo.sli4 + todo.sli5 + todo.sli6) * todo.freq1,
+          body: this.data.bodyPASI * (todo.sli7 + todo.sli8 + todo.sli9) * todo.freq2,
+          leg: this.data.legPASI * (todo.sli10 + todo.sli11 + todo.sli12) * todo.freq3,
         })
       })
     }
